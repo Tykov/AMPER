843828383493b9d24131b5d99adaa7e126aaef86
Evergardener <67442100+Evergardener@users.noreply.github.com>
2020-06-26 21:52:07 +0300

    Added article:publisher meta tag

--- php/amper.php
+++ php/amper.php
@@ -55,8 +55,8 @@ limitations under the License.
 		<meta property="og:url" content="<?php echo $page->permalink(); ?>" />
 		<meta property="og:description" content="<?php echo htmlspecialchars( substr( strip_tags( $page->description() ), 0, 150 ) ) ?>" />
 		<meta property="article:published_time" content="<?php echo date ( 'c', strtotime( $page->dateRaw() ) ) ?>" />
-		echo '<meta property="article:author" content="' . $site->title() . '" />' . PHP_EOL;
-		echo '<meta property="article:publisher" content="@' . $site->title() . '" />' . PHP_EOL;
+		<?php echo '<meta property="article:author" content="' . $site->title() . '" />' . PHP_EOL; ?>
+		<?php echo '<meta property="article:publisher" content="@' . $site->title() . '" />' . PHP_EOL; ?>
 		<?php if ($page->dateModified()): ?>
 		<meta property="article:modified_time" content="<?php echo date ( 'c', strtotime( $page->dateModified() ) ) ?>" />
 		<?php endif ?>
