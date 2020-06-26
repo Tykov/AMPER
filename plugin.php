7fd4e44f307a2d9728b123e614e4ce9076bd8cb0
Evergardener <67442100+Evergardener@users.noreply.github.com>
2020-06-26 09:24:48 +0300

    Initial release

---
+++ plugin.php
@@ -0,0 +1,21 @@
+<?php
+//Based on AMP.DEV html template
+//author: Constantine - evergarden.ru
+//version: 1.0.0
+	class AMPER extends Plugin {
+	public function beforeSiteLoad(){
+	global $url, $page, $site;
+	if(isset($_GET["amp"])){
+		if($url->whereAmI() == "page" && !$url->notFound()){
+			include(__DIR__ . DS . "php" . DS . "amper.php");
+	}
+	}
+	}
+	public function siteHead()
+	{
+	global $page, $url;
+	if($url->whereAmI() == "page" && !$url->notFound()){
+		return '<link href="' . $page->permalink() . '?amp" rel="amphtml" />' . PHP_EOL;
+	}
+	}
+}
