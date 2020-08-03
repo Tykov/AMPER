a9504947409ab2441d36dba245b3419e61021ce7
Evergardener <67442100+Evergardener@users.noreply.github.com>
2020-08-03 18:38:56 +0400

    Optional enable feature

--- plugin.php
+++ plugin.php
@@ -2,10 +2,20 @@
 //Based on AMP.DEV html template
 //author: Constantine - evergarden.ru
 	class AMPER extends Plugin {
+	public function init(){
+	// Custom fields initialization
+	global $site;
+	$userFields = $site->customFields() ?? '{}';
+	if(!isset($userFields['amper'])){
+	$amper_json = json_decode('{"_amper": {"type": "bool","label": "AMPER","tip": "Disable AMP pages for this content"}}', true);
+	$amper_final = json_encode(array_merge($userFields, $amper_json));
+	$site->set(array('customFields'=>$amper_final));
+	}
+	}
 	public function beforeSiteLoad(){
 	global $url, $page, $site;
 	if(isset($_GET["amp"])){
-		if($url->whereAmI() == "page" && !$url->notFound()){
+		if($url->whereAmI() == "page" && !$url->notFound() && $page->custom('_amper') !== true){
 			include(__DIR__ . DS . "php" . DS . "amper.php");
 	}
 	}
@@ -13,7 +23,7 @@
 	public function siteHead()
 	{
 	global $page, $url;
-	if($url->whereAmI() == "page" && !$url->notFound()){
+	if($url->whereAmI() == "page" && !$url->notFound() && $page->custom('_amper') !== true){
 		return '<link href="' . $page->permalink() . '?amp" rel="amphtml" />' . PHP_EOL;
 	}
 	}
